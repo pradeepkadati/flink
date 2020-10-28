10c85a577b38d6a42d6776478a23f03c9403aa48
pradeepkadati <pradeepkadati@gmail.com>
2020-10-28 22:11:45 +0530

    flink: word count setup

---
+++ wordcount/src/main/java/io/javanuts/flink/wordcount/App.java
@@ -0,0 +1,13 @@
+package io.javanuts.flink.wordcount;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
